03ae84b9fd2775d5843b0bcc66a213a9c1338d7c
vitaluska123 <87501597+vitaluska123@users.noreply.github.com>
2022-07-28 03:44:16 +0300

    to json

--- index.php
+++ index.php
@@ -9,26 +9,28 @@
 <body>
 	<?php
 
-	include "video/css-guide-.php";
+	$json = file_get_contents("vid.json");
+	$obj = json_decode($json, true);
+
 
 	function video_Url_to_html($video)
 	{
-		return "<div class=\"video-div\"><iframe class=\"video\" width=\"500\" height=\"315\" src=\"$video\" title=\"YouTube video player\"frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\"allowfullscreen></iframe></div>";
+		return "<div class=\"video-div\"><iframe class=\"video\" width=\"500\" height=\"315\" src=\"https://www.youtube.com/embed/$video\" title=\"YouTube video player\"frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\"allowfullscreen></iframe></div>";
 	}
 	function vidCount($css_guide)
 	{
 		echo "<div class=\"video-count\">Кол-во видео: " . count($css_guide) . "</div>";
 	}
-	function PrintVideo($css_guide)
+	function PrintVideo($obj)
 	{
-		for ($i = 1; $i <= count($css_guide); $i++) {
-			echo video_Url_to_html($css_guide[$i]);
+		for ($i = 1; $i <= count($obj); $i++) {
+			echo video_Url_to_html($obj["video" . $i]);
 		}
 	}
 
 
-	vidCount($css_guide);
-	PrintVideo($css_guide);
+	vidCount($obj);
+	PrintVideo($obj);
 	?>
 
 </body>
--- video/css-guide-.php
+++
@@ -1,2 +0,0 @@
-<?php
-$css_guide[1] = "https://www.youtube-nocookie.com/embed/EuIshKbzG4I";
