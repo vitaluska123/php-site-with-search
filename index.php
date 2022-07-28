a204145b4579fc00f8936745459b94157cfb79f7
vitaluska123 <87501597+vitaluska123@users.noreply.github.com>
2022-07-28 19:06:42 +0300

    sdigjsgdaj

--- index.php
+++ index.php
@@ -8,8 +8,12 @@
 </head>
 
 <body>
+	<form name="search" action="/check.php" method="post">
+		<input type="text" name="searchTxt" placeholder="Поиск">
+		<input type="submit" name="go" value="Найти">
+	</form>
+
 	<?php
-	echo "<form name=\"search\" action=\"/check.php\" method=\"post\"><input type=\"text\" name=\"searchTxt\" placeholder=\"Поиск\"><input type=\"submit\" name=\"go\" value=\"Найти\"></form>";
 	require(__DIR__ . '/php/allVid.php');
 
 	$json = file_get_contents(__DIR__ . '/vid/vid.json');
